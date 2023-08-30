4c84575abc3ae714026a79d56805bd8c48e2245c
Mayur Sunil Chavan <98825289+mayurtechnologys@users.noreply.github.com>
2023-08-30 10:05:07 +0530

    Update HYPERWISOR_IOT.h

--- HYPERWISOR_IOT.h
+++ HYPERWISOR_IOT.h
@@ -4,15 +4,18 @@
 #include <Arduino.h>
 #include <HTTPClient.h>
 
-class HYPERWISOR_IOT {
+class HYPERWISOR_IOT
+{
 public:
-    HYPERWISOR_IOT(String apiKey);
+    HYPERWISOR_IOT();
+    void init(String apiKey);
     String sendData(String data);
     String getsensordata();
     String get_ch_status();
     String updateCH(String CH_NAME, String CH_VALUE);
 
 private:
+    bool initialized;
     String _apiKey;
 };
 
