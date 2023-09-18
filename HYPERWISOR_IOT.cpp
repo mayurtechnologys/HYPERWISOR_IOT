60a1877ad3218c11fe1200b7ddf50fa1c626ba19
Mayur Sunil Chavan <98825289+mayurtechnologys@users.noreply.github.com>
2023-09-18 10:14:04 +0530

    Update HYPERWISOR_IOT.cpp

--- HYPERWISOR_IOT.cpp
+++ HYPERWISOR_IOT.cpp
@@ -3,16 +3,12 @@
 
 HYPERWISOR_IOT::HYPERWISOR_IOT()
 {
-
     initialized = false;
-
-    //_apiKey = apiKey;
 }
 
 void HYPERWISOR_IOT::init(String apiKey)
 {
     _apiKey = apiKey;
-
     initialized = true;
 }
 
@@ -25,32 +21,8 @@ String HYPERWISOR_IOT::sendData(String data)
     int httpPOSTCode = http.POST(startapi);
     String payload = http.getString();
 
-    // Serial.println("HTTP POST Response Code: " + String(httpPOSTCode));
-    // Serial.println("Server Response: " + payload);
-
     http.end();
-    return payload; // Return the server response
-}
-
-String HYPERWISOR_IOT::updateCH(String CH_NAME, String CH_VALUE)
-{
-    String chupdateapi = "https://nikolaindustry.wixsite.com/hyperwisor/_functions/updatech?apikey=" + _apiKey + "&chnumber=" + CH_NAME + "&value=" + CH_VALUE;
-    HTTPClient http;
-    delay(100);
-    http.begin(chupdateapi);
-    int httpCode = http.GET();
-
-    String response;
-
-    if (httpCode > 0)
-    {
-        response = http.getString();
-        // ... Continue with the rest of your function
-    }
-
-    http.end(); // Don't forget to close the connection
-
-    return response; // Return the server response
+    return payload;
 }
 
 String HYPERWISOR_IOT::getsensordata()
@@ -67,38 +39,13 @@ String HYPERWISOR_IOT::getsensordata()
     if (httpCode > 0)
     {
         response = http.getString();
-        // ... Continue with the rest of your function
     }
 
-    http.end(); // Don't forget to close the connection
+    http.end();
 
-    return response; // Return the server response
+    return response;
 }
 
-String HYPERWISOR_IOT::get_ch_status()
-{
-    String getlink = "https://nikolaindustry.wixsite.com/hyperwisor/_functions/devicestatus?apikey=" + _apiKey;
-    HTTPClient http;
-
-    delay(100);
-    http.begin(getlink);
-    int httpCode = http.GET();
-
-    String response;
-
-    if (httpCode > 0)
-    {
-        response = http.getString();
-        // ... Continue with the rest of your function
-    }
-    // Serial.println(getlink);
-    // Serial.println("HTTP getch Response Code: " + String(httpCode));
-    // Serial.println("Server Response: " + response);
-
-    http.end(); // Don't forget to close the connection
-
-    return response; // Return the server response
-}
 
 String HYPERWISOR_IOT::update_schema(String data)
 {
@@ -113,12 +60,11 @@ String HYPERWISOR_IOT::update_schema(String data)
     if (httpCode > 0)
     {
         response = http.getString();
-        // ... Continue with the rest of your function
     }
 
-    http.end(); // Don't forget to close the connection
+    http.end();
 
-    return response; // Return the server response
+    return response;
 }
 
 String HYPERWISOR_IOT::get_schema()
@@ -135,23 +81,19 @@ String HYPERWISOR_IOT::get_schema()
     if (httpCode > 0)
     {
         response = http.getString();
-        // ... Continue with the rest of your function
     }
-    // Serial.println(getlink);
-    // Serial.println("HTTP getch Response Code: " + String(httpCode));
-    // Serial.println("Server Response: " + response);
 
-    http.end(); // Don't forget to close the connection
+    http.end();
 
-    return response; // Return the server response
+    return response;
 }
 
 String HYPERWISOR_IOT::extractor(String data, String key)
 {
-    
+
     char json[1512];
     String payload = data;
-    Serial.println(payload);
+    // Serial.println(payload);
     int leng = payload.length();
     String newpayload = payload.substring(1, leng - 1);
 
@@ -159,19 +101,27 @@ String HYPERWISOR_IOT::extractor(String data, String key)
     newpayload.replace("\n", "");
     newpayload.trim();
     newpayload.remove(0, 10);
-     newpayload.toCharArray(json, 1512);
-    Serial.println(newpayload);
+    newpayload.toCharArray(json, 1512);
+    // Serial.println(newpayload);
     StaticJsonDocument<1512> jsonDoc;
     DeserializationError error = deserializeJson(jsonDoc, newpayload);
 
     if (error)
     {
+
         Serial.print("deserializeJson() failed: ");
         String errorr = error.c_str();
-        Serial.println(error.c_str());
-        return errorr;
+        // // Serial.println(error.c_str());
+        // // return errorr;
     }
-
     String value = jsonDoc[key];
-    return value;
+    if (value == "null")
+    {
+        String errorspace = "0";
+        return errorspace;
+    }
+    else
+    {
+        return value;
+    }
 }
