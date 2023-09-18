24131ec44e78bc39f8b5e6b7bc98ab5236db7403
Mayur Sunil Chavan <98825289+mayurtechnologys@users.noreply.github.com>
2023-09-18 10:13:07 +0530

    Update HYPERWISOR_IOT.h

--- HYPERWISOR_IOT.h
+++ HYPERWISOR_IOT.h
@@ -12,16 +12,14 @@ public:
     void init(String apiKey);
     String sendData(String data);
     String getsensordata();
-    String get_ch_status();
-    String updateCH(String CH_NAME, String CH_VALUE);
     String get_schema();
     String update_schema(String data);
-    String extractor(String data,String key);
-    
+    String extractor(String data, String key);
 
 private:
     bool initialized;
     String _apiKey;
+    
 };
 
 #endif
